93cc9660aed7639197bb12d5327fdc28d040d246
jakedevar <devarennesjacob@gmail.com>
2023-04-14 11:40:48 -0500

    rest url from previous commit to correct url

--- src/utils/resultsAPICall.ts
+++ src/utils/resultsAPICall.ts
@@ -3,8 +3,7 @@ import {ApiCallResult} from "../types/ApiCallResult";
 
 const resultsAPICall = async (caseSensitive: boolean, searchTerm: string, pageNumber: number, quantity: number, exactMatch: boolean): Promise<ApiCallResult> =>  {
   try {
-    // const url = `https://jakedevar-shakesearch.onrender.com/search?caseSensitive=${caseSensitive}&searchTerm=${searchTerm}&pageNumber=${pageNumber}&quantity=${quantity}&exactMatch=${exactMatch}`;
-    const url = "yes"
+    const url = `https://jakedevar-shakesearch.onrender.com/search?caseSensitive=${caseSensitive}&searchTerm=${searchTerm}&pageNumber=${pageNumber}&quantity=${quantity}&exactMatch=${exactMatch}`;
     const response = await axios.get(url);
     const rawResponseObject = response.data;
     const responseObject: ApiCallResult = {
